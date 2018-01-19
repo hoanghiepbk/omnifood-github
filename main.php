44fdde78a4b79e9d8a12c5162bf6a1f7bcb9d60e
Hoang Hiep <hoanghiepbk@gmail.com>
2018-01-19 16:08:04 +0700

    upd

--- main.php
+++ main.php
@@ -352,7 +352,19 @@
                 <h2> We 're happy to hear from you </h2>
             </div>
             <div class="row">
-                <form method="post" action="#" class="contact-form">
+                <form method="post" action="save.php" class="contact-form">
+                <div class="row">
+                    <?php
+                    if($_GET['success'] == 1){
+                        echo "<div class=\"form-messages success\">Thanks you! Your message has been sent </div>";
+                    
+                    }
+                    if($_GET['error'] == -1){
+                        echo "<div class=\"form-messages error\">Something went wrong !Please try again! </div>";
+                    }
+                    ?>
+                    </div>
+
                     <div class="row">
                         <div class="col span-1-of-3">
                             <label for="name">Name</label>
